a68f10fbd97c3bfcbbc83ff558cd7353fe502933
unknown <kmodise9898@gmail.com>
2020-09-13 17:32:05 +0200

    push

--- src/main/java/View/Player.java
+++ src/main/java/View/Player.java
@@ -37,32 +37,73 @@ public class Player {
         else if (!keyLeft && keyRight)
             xSpeed++;
 
-        if (keyUp && keyDown || !keyUp && !keyDown)
-            ySpeed *= 0.8;
-        else if (keyUp && !keyDown)
-            ySpeed--;
-        else if (!keyUp && keyDown)
-            ySpeed++;
+        if (keyUp){
+            hitBox.y++;
+            for(Wall wall: panel.walls){
+                if (wall.hitBox.intersects(hitBox))
+                    ySpeed = -6;
+            }
+            hitBox.y--;
+        }
+        ySpeed += 0.3;
+
+        //x-axis collision
+        hitBox.x += xSpeed;
+        for(Wall wall: panel.walls){
+            if (wall.hitBox.intersects(hitBox)){
+                hitBox.x -= xSpeed;
+                while (!wall.hitBox.intersects(hitBox)){
+                    hitBox.x += Math.signum(xSpeed);
+                }
+                hitBox.x -= Math.signum(xSpeed);
+                xSpeed = 0;
+                x = hitBox.x;
+            }
+        }
+
+
+        //y-axis collision
+
+        hitBox.y += ySpeed;
+        for(Wall wall: panel.walls){
+            if (wall.hitBox.intersects(hitBox)){
+                hitBox.y -= ySpeed;
+                while (!wall.hitBox.intersects(hitBox)){
+                    hitBox.y += Math.signum(ySpeed);
+                }
+                hitBox.y -= Math.signum(ySpeed);
+                ySpeed = 0;
+                y = hitBox.y;
+            }
+        }
+
+
+//        if (keyUp && keyDown || !keyUp && !keyDown)
+//            ySpeed *= 0.8;
+//        else if (keyUp && !keyDown)
+//            ySpeed--;
+//        else if (!keyUp && keyDown)
+//            ySpeed++;
 
         //setting the max speed
         if (xSpeed > 4)
             xSpeed = 4;
         if (xSpeed < -4)
             xSpeed = -4;
-        if (ySpeed > 4)
-            ySpeed = 4;
-        if (ySpeed < -4)
-            ySpeed = -4;
+//        if (ySpeed > 4)
+//            ySpeed = 4;
+//        if (ySpeed < -4)
+//            ySpeed = -4;
 
         //preventing the player from sliding
         if (xSpeed > 0 && xSpeed < 0.75)
             xSpeed = 0;
         if (xSpeed < 0 && xSpeed > -0.75)
             xSpeed = 0;
-        if (ySpeed > 0 && ySpeed < 0.75)
-            ySpeed = 0;
-        if (ySpeed < 0 && ySpeed > -0.75)
-            ySpeed = 0;
+//        if (ySpeed > 0 && ySpeed < 0.75)
+//            ySpeed = 0;
+//        if (ySpeed < 0 && ySpeed > -0.75)
+//            ySpeed = 0;
 
             x+=xSpeed;
             y+=ySpeed;
