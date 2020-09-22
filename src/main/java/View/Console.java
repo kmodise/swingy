c600418655b86dc574beceb5dc922f305566ae88
unknown <kmodise9898@gmail.com>
2020-09-22 17:23:07 +0200

    push

--- src/main/java/View/Console.java
+++ src/main/java/View/Console.java
@@ -1,8 +1,12 @@
 package View;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Scanner;
 
 public class Console {
@@ -16,6 +20,45 @@ public class Console {
         int choice = Integer.parseInt(option.next());
         if (choice == 1){
             System.out.println("creating hero");
+            Scanner getInputObj = new Scanner(System.in);
+            System.out.println("heroName ");
+            String heroName = getInputObj.next();
+            System.out.println("heroClass");
+
+            String heroClass = getInputObj.next();
+            System.out.println("heroLevel");
+
+            String heroLevel = getInputObj.next();
+            System.out.println("heroExperience");
+
+            String heroCExperience = getInputObj.next() ;
+            System.out.println("heroAttack");
+
+            String heroAttack = getInputObj.next();
+            System.out.println("heroDefence");
+
+            String heroDefence = getInputObj.next();
+            System.out.println("heroHitPoints");
+
+            String heroHitPoints = getInputObj.next();
+
+            String hero = heroName + " " + heroClass + " " + heroLevel + " " + heroCExperience + " " + heroAttack + " " + heroDefence + " " + heroHitPoints + "\n";
+            System.out.println(hero);
+
+            try {
+                Files.write(Paths.get("src\\main\\java\\Model\\heroes.txt"), hero.getBytes(), StandardOpenOption.APPEND);
+            }catch (IOException e) {
+                System.out.println(e);
+            }
+
+            String displayHero = "\n" + "   Name ====> " + heroName + "    Class ====> " + heroClass + "   Level ===> " + heroLevel + "   Experience ===> " + heroCExperience + "   Attack ===> " + heroAttack + "  Defence ===> " + heroDefence + "    Hitpoints === > " + heroHitPoints;
+            try {
+                Files.write(Paths.get("src\\main\\java\\Model\\heroesDisplay.txt"), displayHero.getBytes(), StandardOpenOption.APPEND);
+            }catch (IOException e) {
+                System.out.println(e);
+            }
+
+
         }
         else if (choice == 2){
             System.out.println("choose hero");
@@ -29,6 +72,7 @@ public class Console {
         }
         else{
             System.out.println("not a valid choice");
+            System.exit(1);
         }
     }
     
